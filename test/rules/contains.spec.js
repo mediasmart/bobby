e7f295f94878019367097432bb806c4938c30677
Mikel Garcia <mikel_6@hotmail.com>
2017-02-16 15:21:17 +0100

    Add min_length and max_length in the length rule

--- test/rules/contains.spec.js
+++ test/rules/contains.spec.js
@@ -1,49 +1,48 @@
-import { assert, expect } from 'chai';
+import { expect } from 'chai';
 import contains from '../../src/rules/contains';
 
-describe('contains()', function() {
-
+describe('contains()', () => {
   const sampleString = 'Hello World';
   const sampleArray = ['hello', 'world'];
   const sampleNumber = 1980;
 
-  it('Up & Running', function() {
+  it('Up & Running', () => {
     expect(typeof contains).to.equal('function');
   });
 
-  it('accept a valid {string} type on a {string} sample', function() {
+  it('accept a valid {string} type on a {string} sample', () => {
     expect(contains(sampleString, 'World')).to.equal(true);
   });
 
-  it('refuse a invalid {string} type on a {string} sample', function() {
+  it('refuse a invalid {string} type on a {string} sample', () => {
     expect(contains(sampleString, 'world')).to.equal(false);
   });
 
-  it('accept a valid {string} type on a {array} sample', function() {
+  it('accept a valid {string} type on a {array} sample', () => {
     expect(contains(sampleArray, 'world')).to.equal(true);
   });
 
-  it('refuse a invalid {string} type on a {array} sample', function() {
+  it('refuse a invalid {string} type on a {array} sample', () => {
     expect(contains(sampleArray, 'World')).to.equal(false);
   });
 
-  it('accept a valid {array} type on a {array} sample', function() {
+  it('accept a valid {array} type on a {array} sample', () => {
     expect(contains(sampleArray, sampleArray)).to.equal(true);
   });
 
-  it('refuse a invalid {array} type on a {array} sample', function() {
+  it('refuse a invalid {array} type on a {array} sample', () => {
     expect(contains(sampleArray, ['hello', 'mars'])).to.equal(false);
   });
 
-  it('accept a valid {number} type on a {number} sample', function() {
+  it('accept a valid {number} type on a {number} sample', () => {
     expect(contains(sampleNumber, 8)).to.equal(true);
   });
 
-  it('refuse a valid {number} type on a {number} sample', function() {
+  it('refuse a valid {number} type on a {number} sample', () => {
     expect(contains(sampleNumber, 4)).to.equal(false);
   });
 
-  it('refuse a invalid {number} type on a {string} sample', function() {
+  it('refuse a invalid {number} type on a {string} sample', () => {
     expect(contains(sampleString, 1)).to.equal(false);
   });
 });
--- test/rules/exists.spec.js
+++ test/rules/exists.spec.js
@@ -1,20 +1,18 @@
-import { assert, expect } from 'chai';
+import { expect } from 'chai';
 import exists from '../../src/rules/exists';
 
-describe('exists()', function() {
-
+describe('exists()', () => {
   const sample = 'Hello World';
 
-  it('Up & Running', function() {
+  it('Up & Running', () => {
     expect(typeof exists).to.equal('function');
   });
 
-  it('accept a valid value type', function() {
+  it('accept a valid value type', () => {
     expect(exists(sample, true)).to.equal(true);
   });
 
-  it('accept a valid value type', function() {
+  it('accept a valid value type', () => {
     expect(exists(undefined, false)).to.equal(true);
   });
-
 });
